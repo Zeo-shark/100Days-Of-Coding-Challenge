4a06f17e8605295884a3795dbfe45186ef56a4d5
saurav <sauravberaiit@gmail.com>
2020-10-18 23:12:35 +0530

    added a multline comment

--- Java_Learning/Tutorial1.java
+++ Java_Learning/Tutorial1.java
@@ -1,6 +1,9 @@
 public class Tutorial1{
     public static void main(String[] args) {
         //show java "tool"
+        /*The Devfest is fantastic and coding is awesome
+        I wanna be a CS Geek.
+        */
         System.out.println("Hello code");
     }
 }
