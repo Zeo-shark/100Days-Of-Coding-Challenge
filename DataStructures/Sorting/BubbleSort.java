1b9694d06fae1bc5ad1a397569564010f74f4206
saurav <sauravberaiit@gmail.com>
2020-11-07 00:00:28 +0530

    added insertionSort

--- DataStructures/Sorting/BubbleSort.java
+++ DataStructures/Sorting/BubbleSort.java
@@ -4,6 +4,12 @@ class BubbleSort
         int n= arr.length;
         for( int i=0; i<n-1; i++)
             for(int j=0; j<n-i-1; j++)
+            if(arr[j]<arr[j+1])
+            {
+                int temp= arr[j];
+                arr[j]= arr[j+1];
+                arr[j+1] = temp;
+            }
     }
 
 
@@ -13,6 +19,8 @@ class BubbleSort
         System.out.println(arr[i]+" ");
     System.out.println();
     }
+    
+    //Driver method to  test above
     public static void main(String[] args) {
         
         BubbleSort obj= new BubbleSort();
---
+++ DataStructures/Sorting/Insertion.java
@@ -0,0 +1,35 @@
+public class Insertion{
+
+
+    void Sort(int arr[]){
+        int n= arr.length;
+
+        for(int i=1;i<n;i++){
+
+            int key =arr[i];
+            int j=i-1;
+
+            while(j>=0 && arr[j]>key)
+            {
+                arr[j+1]=arr[j];
+                j= j-1;
+            }
+
+            arr[j+1]= key;
+        }
+    }
+    void printArray(int arr[]){
+        int n= arr.length;
+        for(int i=0;i<n;++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[]={22, 34, 56,11, 10,12 , 6, 1};
+        Insertion obj= new Insertion();
+        obj.Sort(arr);
+        System.out.println("Sorted Arrays");
+        obj.printArray(arr);
+    }
+}
